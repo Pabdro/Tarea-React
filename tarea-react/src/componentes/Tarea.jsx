203545801031766c9f97266bc44e0224797fd1ff
Pabdro <pablis.badani@gmail.com>
2023-05-25 13:48:44 -0400

    componente tarea

---
+++ tarea-react/src/componentes/Tarea.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import Bien from '../assets/bien.png'
+import Mal from '../assets/mal.png'
+import './Tarea.css'
+const Tarea = () => {
+    const [isWordCrossed, setIsWordCrossed] = useState(false);
+    const [isContainerVisible, setIsContainerVisible] = useState(true);
+    const handleGoodButtonClick = () => {
+        setIsWordCrossed(true);
+    };
+    const handleBadButtonClick = () => {
+        setIsContainerVisible(false);
+      };
+    
+      if (!isContainerVisible) {
+        return null;
+      }
+    return (
+        <div>
+            <div className="container">
+            <h1 className={isWordCrossed ? 'crossed' : ''}>Palabra</h1>
+            <div className="buttons">
+                <button className="good-button" onClick={handleGoodButtonClick}>
+                <img src={Bien} alt="Botón Bien" />
+                </button>
+                <button className="bad-button" onClick={handleBadButtonClick}>
+                 <img src={Mal} alt="Botón Mal" />
+             </button>
+            </div>
+            </div>
+        </div>
+    )
+}
+export default Tarea; 
